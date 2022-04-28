0624ee81f05a5736a8cb6cdf8917f55a27e41312
Aneurin Barker Snook <a@aneur.in>
2022-04-28 16:28:10 +0100

    feat: add basic api

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,18 @@
 import config from './config'
 import getTargets from './targets'
+import listen from './api'
 import { Log, LogLevelFromString, StdioAdaptor } from '@edge/log'
 import { clearInterval, setInterval } from 'timers'
 import request, { Response } from './request'
 
-const main = async () => {
-  const adaptors = [new StdioAdaptor()]
-  const log = new Log(adaptors)
-  log.setLogLevel(LogLevelFromString(config.log.level))
-  const requestLog = log.extend('request')
-
-  log.info('starting')
+const doRequests = async (log?: Log): Promise<void> => {
   const targets = await getTargets()
+  const doRequest = request(log)
 
   const requests = targets.map((t, i) => {
     const ms = i * config.delay
     return () => new Promise<Response>((resolve, reject) => {
-      setTimeout(() => request(requestLog)(t).then(resolve).catch(reject), ms)
+      setTimeout(() => doRequest(t).then(resolve).catch(reject), ms)
     })
   })
 
@@ -25,32 +21,44 @@ const main = async () => {
   let locked = false
   const tick = async () => {
     if (locked) {
-      log.warn('previous requests not completed, skipping tick')
+      log?.warn('previous requests not completed, skipping tick')
       return
     }
     locked = true
 
-    log.info('cycle')
     cycles++
     if (config.stop > 0 && cycles >= config.stop) {
       if (i) clearInterval(i)
     }
 
-    try {
-      const results = await Promise.allSettled(requests.map(r => r()))
-      results.forEach(r => console.log(r))
-    }
-    catch (err) {
-      log.error('critical error', { err })
-      if (i) clearInterval(i)
-    }
-    finally {
-      locked = false
-    }
+    log?.info('sending requests', { num: requests.length })
+    const results = await Promise.allSettled(requests.map(r => r()))
+    const ok = results.filter(r => r.status === 'fulfilled' )
+    log?.info('completed requests', { num: ok.length, errors: requests.length - ok.length })
+    ok.forEach(r => console.log(r))
+
+    locked = false
   }
 
   i = setInterval(tick, config.frequency)
   tick()
+}
+
+const main = async () => {
+  const adaptors = [new StdioAdaptor()]
+  const log = new Log(adaptors)
+  log.setLogLevel(LogLevelFromString(config.log.level))
+
+  log.info('starting')
+  try {
+    await Promise.all([
+      doRequests(log.extend('request')),
+      listen(log.extend('http'))
+    ])
+  }
+  catch (err) {
+    log.error('critical error', { err })
+  }
 }
 
 main()
